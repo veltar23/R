71b488f616ebf2439301091d7727aac7157fe3ff
veltar23 <r05741051@ntu.edu.tw>
2017-04-12 11:06:42 +0800

    170412

--- hw2/1.R
+++ hw2/1.R
@@ -3,18 +3,16 @@ library(XML)
 library(RCurl)
 library(httr)
 
-urlPath <- "https://www.ptt.cc/bbs/elderly/index33.html"
-temp    <- getURL(urlPath, encoding = "big5")
+urlPath <- "https://www.ptt.cc/bbs/Bio-Job/index.html"
+temp    <- getURL(urlPath, encoding = "UTF-8")
 xmldoc  <- htmlParse(temp)
 title   <- xpathSApply(xmldoc, "//div[@class=\"title\"]", xmlValue)
+title   <- gsub("\n", "", title)
+title   <- gsub("\t", "", title)
 author  <- xpathSApply(xmldoc, "//div[@class='author']", xmlValue)
 date    <- xpathSApply(xmldoc, "//div[@class='date']", xmlValue)
 response<- xpathSApply(xmldoc, "//div[@class='nrec']", xmlValue)
 
 alldata <- data.frame(title, author, date, response)
 
-write.table(alldata,"pttelderly.csv") #你會在此Script資料夾下得到一個.csv
-
-library(knitr)
-data=read.table("pttelderly.csv")
-kable(data)
+write.table(alldata,"biojob.csv") #你會在此Script資料夾下得到一個.csv
